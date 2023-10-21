c5c457be3202a1d41780d3137e51f82c320fb824
Yosef <hfantasy00@gmail.com>
2023-10-21 19:15:18 -0400

    Labeled Characters

---
+++ MysterymemoryThief/Chapter 1/Chapter1.swift
@@ -0,0 +1,14 @@
+//
+//  Chapter1.swift
+//  CLIStoryTemplate
+//
+
+import Foundation
+
+let heroName = "Yosef Albeketawi"
+let villianName = "Ali Alsaraf"
+let sideCameoMentors = ["Marcus, Nick G, Ty, Kory"]
+func chapterOne() {
+    
+    
+}
