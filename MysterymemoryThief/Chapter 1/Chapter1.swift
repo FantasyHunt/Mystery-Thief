ab7fd37519fb4852220e12ff002af7fa8d116e30
Yosef <hfantasy00@gmail.com>
2023-10-25 10:03:27 -0400

    Small Edit

--- MysterymemoryThief/Chapter 1/Chapter1.swift
+++ MysterymemoryThief/Chapter 1/Chapter1.swift
@@ -5,10 +5,16 @@
 
 import Foundation
 
-let heroName = "Yosef Albeketawi"
+let heroName = "Yosef"
 let villianName = "Ali Alsaraf"
 let sideCameoMentors = ["Marcus, Nick G, Ty, Kory"]
+    .randomElement()
+let locationOne = "Old Red Barn"
+
 func chapterOne() {
+    print("The sound of a car engine approacing slowly comes to halt infront of a \(locationOne) ")
+    print("\(heroName) opens the car door, putting one foot out then the other planting them in the dirt")
+    
     
     
 }
