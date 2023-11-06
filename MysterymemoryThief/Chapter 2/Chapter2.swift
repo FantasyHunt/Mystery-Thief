1d26f2e4fbb36feb2c4725fac2e21515093576b3
marjorie0627 <ab2808@wayne.edu>
2023-11-06 11:08:19 -0500

    Add chapter 2

--- MysterymemoryThief/Chapter 2/Chapter2.swift
+++ MysterymemoryThief/Chapter 2/Chapter2.swift
@@ -5,6 +5,27 @@
 
 import Foundation
 
+
+let locationNameOne: String = "bedroom"
+let locationNameTwo: String = "backyard"
 func chapterTwo() {
-    // Your portion of the story goes here
+    print("\(heroName) went to her \(locationNameOne) to fall asleep in her bed, when she woke up her memory was loss. Then she ran into the \(locationNameTwo) crying continuously, while swinging on her swing. As it turns out her \(villianName) was temporarily. \(heroName) never made fun of no one ever again with a disablity, especially \(villianName) victims.")
+    let percentage = 0.2
+    print (percentage.formatted(.percent))
 }
+
+// adding a thing so there's a change
+
+
+
+
+//  The father comes home from his cleaning job at the hospital and alert his wife of what's going on at the hospital.
+//  The father states to his wife that there is something suspicious going on at work in the doctor's office.  The doctor at the hospital is doing something to his patiences.
+//  Once the patiences see the doctor, they leave his office incoherent, lethargic, and confused.
+//  As the father is telling this story to his wife, there is a knock at the door.  He looks through the peephole and notices the doctor standing there.
+//  The man panics and begins to hide his two children, 15 & 12, in the living room closet, where the coats hang.
+//  Then, he lets the doctor in.  The doctor comes in with his emergency bag and pulls out a syringe and sticks the man and his wife in their necks, numbing them.
+//  They both pass out with their daugther witnessing the whole thing through the closet keyhole.
+//  The doctor leaves the house and the 15 year old daugther, along with her 12 year old brother, comes rushing out of the closet.
+//  The daughter runs to the phone and dials 911 for help, while her brother rounds up the neighbors to find out where they can stay through the night.(JOSIE LINE 29 IS URS TO CHANGE)
+
