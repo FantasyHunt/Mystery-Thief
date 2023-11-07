5f8fc1022041e3838193c4b50e90759418732e4c
FantasyHunt TecSeb marjorie0627 <hfantasy00@gmail.com sebastian.gutierrez60@gmail.com majenkinstutorials@gmail.com>
2023-11-07 09:21:09 -0500

    added loop to the story and array as well.

--- MysterymemoryThief/Chapter 3/Chapter3.swift
+++ MysterymemoryThief/Chapter 3/Chapter3.swift
@@ -5,15 +5,29 @@
 
 import Foundation
 
+
 struct Person {
     var clothes: String
     var shoes: String
     var characterType : String
     var name : String
-
-
-    
 }
+let resouces = ["boat" , "scuba"]
+let randomResources = resouces.randomElement() ?? ""
+//
+
+
+
+
+func chapterThree1() {
+    
+    if let resources = resouces.randomElement() {
+        print("good thing they had a \(resources)")
+    }
+}
+    
+
+
 func dressing  (){
     let heroName = Person(clothes: "Dress", shoes: "boots", characterType: "Hero", name: "Neriah")
     let other = Person(clothes: "T-Shirts", shoes: "sneakers", characterType:"villian name", name: "Monster Cookie")
@@ -31,15 +45,24 @@ let sideCameoMentors2 = "ZayZay"
 let parentName = "Emiliano and Josie"
 let locationOne2 = "Under the sea"
 
-func chapterThree() {
-    print("\(locationOne)")
+func chapterThree2() {
+    print("\(locationOne2)")
     print("\(heroName)")
     print("\(String(describing: sideCameoMentors))")
     print("\(String(describing: villianName))")
     print("\(String(describing: parentName))")
-    print("\(sideCameoMentors) and \(heroName) woke up out of there sleep, there \(parentName) was not at home but heard about the \(villianName) and how he been taking people's memory and leaving the kids behind. So they heard the \(villianName) was under the water in a cave. \(heroName) and \(sideCameoMentors) packed things they would need to go under water, good thing they had a boat and scuba gear and scuba outfits. And a trap to catch the \(villianName). So they come up with a plan to get there \(parentName) back. As they went to get ready to leave \(locationOne). ")
+    print("\(sideCameoMentors2) and \(heroName2) woke up out of there sleep, to see there parents wasnt around \(parentName) was not at home but heard about the \(villianName2) and how he been taking people's memory and leaving the kids behind. So they heard the \(villianName2) was under the water in a cave. \(heroName2) and \(sideCameoMentors2) packed things they would need to go under water, good thing they had a boat and scuba gear and scuba outfits. And a trap to catch the \(villianName2). So they come up with a plan to get there \(parentName) back. As they went to get ready to leave \(locationOne2). ")
     
-    
+    var countdown = 10
+
+    while countdown > 0 {
+        print("\(countdown)...")
+        countdown -= 1
+    }
+        
+        print("Blast off!")
+
+
     
     
     
