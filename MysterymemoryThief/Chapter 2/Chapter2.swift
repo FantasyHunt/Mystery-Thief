644e65262c38012c36728856b5f10a04a676d7b9
marjorie0627 <ab2808@wayne.edu>
2023-11-08 08:53:38 -0500

    Made changes to Chaper 2

--- MysterymemoryThief/Chapter 2/Chapter2.swift
+++ MysterymemoryThief/Chapter 2/Chapter2.swift
@@ -6,12 +6,35 @@
 import Foundation
 
 
-let locationNameOne: String = "bedroom"
-let locationNameTwo: String = "backyard"
+let locationNameOne: String = "hospital"
+let locationNameTwo: String = "living room"
+let locationThree: String = "home"
+let doctorName: String = "Dr. Mendoza"
+let patientsConditions: Array = ["incoherent", "lethargic", "and confused"]
+var age1: Int = 12
+var age2: Int = 15
+var knock: Bool = true
+func openDoor() {
+    if knock {
+        print("The Emiliano opens the door.")
+    }
+}
 func chapterTwo() {
-    print("\(heroName) went to her \(locationNameOne) to fall asleep in her bed, when she woke up her memory was loss. Then she ran into the \(locationNameTwo) crying continuously, while swinging on her swing. As it turns out her \(villianName) was temporarily. \(heroName) never made fun of no one ever again with a disablity, especially \(villianName) victims.")
-    let percentage = 0.2
-    print (percentage.formatted(.percent))
+//    print("\(heroName) went to her \(locationNameOne) to fall asleep in her bed, when she woke up her memory was loss. Then she ran into the \(locationNameTwo) crying continuously, while swinging on her swing. As it turns out her \(villianName) was temporarily. \(heroName) never made fun of no one ever again with a disablity, especially \(villianName) victims.")
+//    let percentage = 0.2
+//    print (percentage.formatted(.percent))
+    
+    //add more here
+    
+  print("Emiliano comes \(locationThree) from his cleaning job at the \(locationNameOne) and alert his wife of what's going on at the \(locationNameOne).")
+  print("The father states to his wife that there is something suspicious going on at work in \(doctorName)'s office.  He is doing something to his patiences.")
+  print("Once his patiences sees him, they leave his office \(patientsConditions).")
+  print("As the father is telling the story to his wife, there is a knock at the door.  Emiliano looks through the peephole and notices \(doctorName) standing there. \(openDoor())")
+  print("Emiliano panics and begins to hide his two children, \(age2) & \(age1), in the \(locationNameTwo) closet, where their coats hung.")
+  print("Then, he lets \(doctorName) in.  The doctor shows up with his emergency bag and pulls a syringe from it and sticks \(parentName) in their necks, numbing them.")
+  print("They both pass out with their daugther witnessing the whole thing through the closet keyhole.")
+  print("The doctor leaves the house and Neriah age \(age2), along with her \(age1) year old brother ZayZay, comes rushing out of the closet.")
+  
 }
 
 // adding a thing so there's a change
@@ -19,13 +42,13 @@ func chapterTwo() {
 
 
 
-//  The father comes home from his cleaning job at the hospital and alert his wife of what's going on at the hospital.
-//  The father states to his wife that there is something suspicious going on at work in the doctor's office.  The doctor at the hospital is doing something to his patiences.
-//  Once the patiences see the doctor, they leave his office incoherent, lethargic, and confused.
-//  As the father is telling this story to his wife, there is a knock at the door.  He looks through the peephole and notices the doctor standing there.
-//  The man panics and begins to hide his two children, 15 & 12, in the living room closet, where the coats hang.
-//  Then, he lets the doctor in.  The doctor comes in with his emergency bag and pulls out a syringe and sticks the man and his wife in their necks, numbing them.
+//  Emiliano comes home from his cleaning job at the hospital and alert his wife of what was going on at the hospital.
+//  The father states to his wife that there is something suspicious going on at work in Dr. Mendoza's office.  He is doing something to his patiences.
+//  Once his patiences sees him, they leave his office incoherent, lethargic, and confused.
+//  As the father is telling the story to his wife, there is a knock at the door.  He looks through the peephole and notices Dr. Mendoza standing there.
+//  The man panics and begins to hide his two children, 15 & 12, in the living room closet, where their coats hung.
+//  Then, he lets the doctor in.  The doctor shows up with his emergency bag and pulls a syringe from it and sticks Emiliano and Josie in their necks, numbing them.
 //  They both pass out with their daugther witnessing the whole thing through the closet keyhole.
-//  The doctor leaves the house and the 15 year old daugther, along with her 12 year old brother, comes rushing out of the closet.
+//  The doctor leaves the house and Neriah age 15, along with her 12 year old brother ZayZay, comes rushing out of the closet.
 //  The daughter runs to the phone and dials 911 for help, while her brother rounds up the neighbors to find out where they can stay through the night.(JOSIE LINE 29 IS URS TO CHANGE)
 
